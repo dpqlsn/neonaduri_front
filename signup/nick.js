7bc8a4477ff508b7fe701f75361165fb051bdbcb
dpqlsn <a01037111209@gmail.com>
2024-08-12 09:24:11 +0900

    FIX :: 자동완성 시 뜨는 파랑 배경화면 없애기

--- signup/nick.js
+++ signup/nick.js
@@ -1,6 +1,8 @@
 const nicknameInput = document.getElementById('nickname');
 const closeButton = document.getElementById('close');
+const successModal = document.getElementById('successModal');
 const loadingScreen = document.getElementById('loadingScreen');
+const closeModalButton = document.getElementById('closeModal');
 
 nicknameInput.addEventListener('input', function() {
     if (nicknameInput.value.trim() !== '') {
@@ -20,10 +22,15 @@ closeButton.style.cursor = 'not-allowed';
 
 closeButton.addEventListener('click', function() {
     if (nicknameInput.value.trim() !== '') {
-        loadingScreen.style.display = 'flex';
-
-        setTimeout(function() {
-            window.location.href = '///Users/dpqls/Desktop/gomdoliro_pj/login/index.html';
-        }, 1000);
+        successModal.style.display = 'flex';
     }
+});
+
+closeModalButton.addEventListener('click', function() {
+    successModal.style.display = 'none';
+    loadingScreen.style.display = 'flex';
+
+    setTimeout(function() {
+        window.location.href = '///Users/dpqls/Desktop/gomdoliro_pj/login/index.html';
+    }, 1000);
 });
